4503994dc2cf1cc8ca3df4e036e4513db5ac62c5
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-11-06 12:18:54 -0300

    att

--- upload-arquivo/server.js
+++ upload-arquivo/server.js
@@ -1,5 +1,30 @@
 const express = require('express')
-const app = express();
+	, app = express()
+	, multer = require('multer');
+
+// cria uma instância do middleware configurada
+// destination: lida com o destino
+// filenane: permite definir o nome do arquivo gravado
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        // error first callback
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+
+        // error first callback
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+// utiliza a storage para configurar a instância do multer
+const upload = multer({ storage });
 
 app.use(express.static('public'));
+
+// continua do mesma forma 
+app.post('/file/upload', upload.single('file'), 
+    (req, res) => res.send('<h2>Upload realizado com sucesso</h2>'));  
+
 app.listen(3000, () => console.log('App na porta 3000'));
